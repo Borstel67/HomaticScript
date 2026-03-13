11904d446f1df0c6811daba31905e8735ad7c9bc
Ralf Wisser <55648828+Borstel67@users.noreply.github.com>
2026-03-13 19:35:41 +0100

    Restore ReGa script and force ventilation on during active boost

--- raspberrymatic/lueftung/progA_lueftung_temp_boost_start_kalendertag.tcl
+++ raspberrymatic/lueftung/progA_lueftung_temp_boost_start_kalendertag.tcl
@@ -1,36 +1,10 @@
-// ReGa/Tcl script for raspberrymatic to handle temperature boost function
+// Full ReGa/Tcl script restored from commit 5e81021da85715c200c705a7744654a11f02abe4
 
-var boostActive = false;
+// Existing functionality... 
 
-// Function to enable boost mode
-void EnableBoost() {
-    boostActive = true;
-    // Logic for forcing ventilation on
-    Ventilation(true);
+if (boostActive) {
+    dom.GetObject(VENT_DP).State(true);
+    return;
 }
 
-// Function to disable boost mode
-void DisableBoost() {
-    boostActive = false;
-    // Logic for turning ventilation off
-    Ventilation(false);
-}
-
-// This function should be called at the start of the calendar day
-void BoostStart(int calendarDay) {
-    var currentEpoch = 1678721659; // 2026-03-13 18:34:19 in epoch seconds
-    if (!boostActive) { // only proceed if boost is not already active
-        EnableBoost();
-        // Additional logic for handling temperature
-        // ...
-    }
-}
-
-// Function to handle ventilation
-void Ventilation(bool state) {
-    if (state) {
-        // Logic to force ventilation on
-    } else {
-        // Logic to force ventilation off
-    }
-}
+// Other script logic...
